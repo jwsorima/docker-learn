c0465c0f65936721cf45475023dd3dae19f774dc
jws <jwsorima@gmail.com>
2024-11-02 20:41:30 +0800

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default (({ mode }) => {
+export default (async ({ mode }: { mode: string }): Promise<UserConfig> => {
   const env = loadEnv(mode, process.cwd());
 
   const usePolling = env.VITE_USE_POLLING === 'true';
